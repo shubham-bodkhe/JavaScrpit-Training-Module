7a0e3e6aca270ec30f4e2e6866548e6bd2632460
Shubham Bodkhe <shubhamb@bsf.io>
2023-03-10 17:58:32 +0530

    The main branch Initial

---
+++ Variable&Statement.js
@@ -0,0 +1,11 @@
+var a = "Shubham";
+//Block scope
+console.log(a);
+
+let b = 10;
+// function scope can resign
+console.log(b);
+
+const c = 1.2;
+// function scope and can't declare and reasign
+console.log(c);
